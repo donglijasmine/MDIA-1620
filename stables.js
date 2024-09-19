2a7d0aca10222251713315532011d287f733908a
Liang Dongli <liangdongli@Liangs-MacBook-Pro.local>
2024-09-19 15:42:19 -0700

    initialize stables and add a horse

---
+++ stables.js
@@ -0,0 +1,13 @@
+console.log("horses are great!")
+let horseName = "Coolguy"; // this is a string - notes for me 
+horseName = "Charlie"; // for strings, prefer double quote
+horseAge = 5;
+console.log(horseName);
+console.log(horseAge);
+let isHorseInStable = false;
+console.log(isHorseInStable);
+isHorseInStable = false;
+console.log (isHorseInStable)
+console.log (horseName);
+horseName = "Coolhorse"
+console.log(horseName)
