f375e959176d0fb90318d02fabfc08aca5a4421f
Liang Dongli <liangdongli@Liangs-MacBook-Pro.local>
2024-09-26 14:38:11 -0700

    add operators to my stable

--- stables.js
+++ stables.js
@@ -7,4 +7,30 @@ console.log(ageNum);
 let isHungry = true; // This is a boolean. 
 console.log(isHungry);
 
-console.log("im hungry")
+console.log("im hungry");
+
+console.log (typeof 890);
+console.log (typeof "890");
+console.log (typeof true);
+console.log (typeof fakevar);
+
+let horseName = "Charlie" // This value can be updated
+
+//const horseName = "Charlie"  // This value can't be updated
+
+//let camelCaseExample; // use camelCase for variables
+
+const STABLE_MONTHLY_FEE = 100; // use uppercase for constants
+
+console.log(STABLE_MONTHLY_FEE);
+console.log(STABLE_MONTHLY_FEE - 20);
+console.log(STABLE_MONTHLY_FEE + 80);
+console.log(STABLE_MONTHLY_FEE * STABLE_MONTHLY_FEE);
+console.log(STABLE_MONTHLY_FEE / 2 );
+
+let catName = "snowball"
+let catNum = 5;
+console.log (catName)
+console.log (catNum)
+let stableIntroduction = `Welcome to "${horseName}'s" stable`; 
+let stableIntroduction = "Welcome to " + horseName + " stable";
