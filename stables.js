b9fe956f5755df7cfb48557e3d97b414f65466de
dongli <liangdongli001@gmail.com>
2024-11-28 11:16:53 -0800

    cleaned codes and added assignment instruction

--- stables.js
+++ stables.js
@@ -1,103 +1,128 @@
-let nameVar = "dongli" // This is a string. nameVar is a variable?
-console.log(nameVar);
+// Welcome!
+//
+//                                                  #    #
+//                                             %%% ##   ##
+//                                          %%%%% ###%🎀###
+//                                         %%%%% ### %%% #
+//                                       %%%%%% ### %%% ###
+//                                        %%%% ## %% #######
+//                                       %%%%% # %% #@#####
+//                                     %%%%%% # % #########
+//                                    %%%%% ##### #########
+//                          ###        %% ####### #########
+//                 %%% ############    ########### ########
+//              %%%% ############################### #######
+//            %%%%% ################################## ######
+//          %%%%%% #################################### #C###
+//         %%%🌸%% #####################################  ###
+//         %%%%% #######################################
+//        %%%%%% ############### BEANS ##################
+//     % %%%%%%% ############### BEANS ##################
+//      %%%%%%%%% ############## BEANS ##################
+//     %%%%%%%%%% ########################################
+//  %%% %%%%%%%%   ###### ################################
+//    %%%%%%%%      ###### #################### ##########
+// % %%%🌸%%%%        ####### ########### ###### ##########
+//  %%%%%%%%%         #######  ########### ###### ########
+// %%%%%%%%%%          ##### ###  ######### ####### ######
+//  %%%%%%%%%%          #### ##               ####### ####
+//  %🌸%%%%%%%%%           ## #                  ##### ###
+//   %%  %% % %%         # ##                      ## ###
+//     %   %    %        # ###                      # ###
+//                        # ###                     ## ###
+//                        # ###                     ## ###
+//                        # ####                   #### ##
+//                       ### ###                  ##### ###
+//                      ####  ###                 ####   ##
+//                     #####   ###                 ##    ##
+//                    #####    ####                      ###
+//                     ##        ###                     ###
+//                                ####                     ##
+//                                 ####                    ###
+//                                                         ####
+//                                                          ##
+//
+// This is a template for the final project. You'll be setting up a stable and making
+// sure it runs smoothly.
+//
+// You can copy and paste this into your stables.js file, and then use it to guide you.
+// Please do not create a new file!
+// Feel free to keep the headings (e.g.: "Variables"), but please delete any boilerplate
+// comments. (Your own comments, where necessary, are acceptable.)
+//
+// NOTE: UNLESS SPECIFIED, VARIABLE NAMING IS UP TO YOU. THERE SHOULD BE NO HARD-CODED
+// NUMBERS OR STRINGS WHERE VARIBALES WOULD BE PREFERRED.
 
-let ageNum = 20; // This is a number. 
-console.log(ageNum);
+//------------------------- Seting up shop -------------------------//
 
-let isHungry = true; // This is a boolean. 
-console.log(isHungry);
+// Initialize a variable called "horses" that will store the horses in your stable.
+// Create a variable and use it to store a message visitors to the stable.
+// Create a variable and use it to store the cost of a late payment fee.
+// Create a variable and use it to store the number of available stalls in the stable.
 
-console.log("im hungry");
+//------------------------- First day -------------------------//
 
-console.log (typeof 890);
-console.log (typeof "890");
-console.log (typeof true);
-console.log (typeof fakevar);
+// Using a constructor, add at least 3 horses to your stables.
+//
+// The horse should have the following properties:
+// - name, nickname, favorite treat (string)
+// - age, monthly rent (number)
+// - location (boolean inside/outside)
+// - two unique properties of your choice (use any primitive)
+// - a method that introduces your horse and references at least 3 object properties.
+// - a second, unique method of your choice
 
-let horseName = "Charlie" // This value can be updated
+// Store the horses you've just created in your "horses" variable.
 
-//const horseName = "Charlie"  // This value can't be updated
+// Fancy! Another horse wants to be stabled! Create a variable that stores an object
+// literal of your new horse, and add it to your "horses" variable.
 
-//let camelCaseExample; // use camelCase for variables
+// Initialize new property to your horses: a boolean that allows you to check if your horse
+// is hungry or not. Use dot notation.
 
-const STABLE_MONTHLY_FEE = 100; // use uppercase for constants
+//------------------------- Stable roster -------------------------//
 
-console.log(STABLE_MONTHLY_FEE);
-console.log(STABLE_MONTHLY_FEE - 20);
-console.log(STABLE_MONTHLY_FEE + 80);
-console.log(STABLE_MONTHLY_FEE * STABLE_MONTHLY_FEE);
-console.log(STABLE_MONTHLY_FEE / 2 );
+// We want to keep track of our horses and how they are doing. Paste the data structure
+// of our horse variable below so we can reference it. It is okay to comment it out.
 
-let catName = "snowball"
-let catNum = 5;
-console.log (catName)
-console.log (catNum)
-let stableIntroduction = `Welcome to "${horseName}'s" stable`; 
-console.log (stableIntroduction)
+//---------------------- Growing business ----------------------//
 
-////// LESSON 4 CODE ///////
-let horseNickName ="Charming";
-let horseAge = 5;
-let horseType = "bahkir";
-let horseColor = "brown"
-let horseIntroduction = `My horse is called "${horseNickName}". He is ${horseAge}. His type is ${horseType}, and his color is ${horseColor}.`
-console.log(horseIntroduction)
-let Promo = `You would save ${STABLE_MONTHLY_FEE * 3 * 0.1} with a 10% discount applied to a 3 month stay at your stables.`
-console.log (Promo)
+// We've added some horses! Re-assign the variable that contains the number of available stalls.
+// Use the math operator to determine how many stables you should have left, given the number of
+// horses you've added.
 
-//////LESSON 5 CODE//////
-let word = "Rainbow";
-console.log (word.charAt(0));
+// Express the following in code: If the variable that contains your remaining stalls
+// is less than 2, log out "We need to build more stalls", otherwise log out "We have
+// [number of stalls] available!"
 
-//////LESSON 6 CODE//////
+// Create a function that logs out how much will an individual horse will owe if rent is
+// paid late. Invoke the function.
 
-let isHorseInside=true
-let horseOne=[horseName, horseAge, isHorseInside, horseNickName];
-let horseTwo=["strawberry", 2, false, "Pie"];
+// Add loop that checks to find out if a certain horse likes a treat. If the horse does not like
+// the treat, log out their disatisfaction and check the next horse. Exit the loop when you find
+// the horse that likes the treat.
 
-console.log(horseOne);
-console.log(horseOne[0]+"'s nickname is" + horseOne[3]);
-console.log(horseTwo);
-console.log(`${horseTwo[0]}'s nickname is ${horseTwo[3]}`);
+// Create and invoke a function that returns (not logs) the nickname of a chosen horse. Log out
+// the return value outside of the function.
 
-console.log(horseOne[0]+ [3]);
-horseOne.push("blue");
-console.log(horseOne);
-console.log(horseOne [1] + horseTwo [1]);
+//------------------------- Day to day operations -------------------------//
 
-if (horseOne [1] === 3) {
-    console.log(horseOne[0]+" is 3");
-} else {
-    console.log(horseOne[0]+ " is NOT 3");
-}
-console.log(
-    horseOne[0] + " and " + horseTwo[0] + " are staying at the stables"
-);
+// Create and invoke a function references one of your unique horse properties. It should
+// contain a conditional or a loop.
 
-let horseNames=[horseName, "Strawberry", "Beans"];
-let horseIntro = `There are ${horseNames.length} horses staying at my stables: ${horseNames[0]}, ${horseNames[1]}, ${horseNames[2]}!`;
-console.log(horseIntro);
-///// the horseIntro code above is a better than the one below one because it is easier for reading and more efficient //////
-let differentHorseIntro=
-"There are " +
-horseNames.length +
-" horses staying at my stables:" +
-horseNames[0] +
-"," +
-horseNames[1] +
-"," +
-horseNames[2] +
-"!";
+// Morning! Create and invoke a function that moves all your horses outside. It should
+// log out statement indicating the horses have been moved to spend time in the sun.
 
-console.log(differentHorseIntro);
+// Initialize a method on your horses that moves them inside if they are outside, and
+// vice versa.
 
-// LAB TODAY - branch lesson-6 done by Sunday
-// Stables.js
-// - Output an introduction to your horses
-// - Output whether your horses are inside or outside at least 3 horses!!!
-// Readme
-// 3 confusing things
-// 3 things you feel like you have a solid understanding of
-// Analogies that you use to think about things (gif)
+// Food! Create and invoke a function that feeds your horses. If the horse is outside, it
+// should call them in, and then all horses should be fed a treat. Log the activity of
+// the horses.
 
+// Bedtime! Update the function that lets your horses outside so that it also calls them
+// in if it is getting dark. Only the horses that moved should log out a message. Invoke the function.
 
+//------------------------- Bonus -------------------------//
+
+// Replace the ascii art at the top of the file with art representing your stable.
